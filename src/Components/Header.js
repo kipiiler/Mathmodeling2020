261a9fe71eff149f4d12f523ee9848e758712888
hoanghohohaha <hoanghohohaha@gmail.com>
2020-07-24 00:11:50 +0700

    .

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,7 +8,9 @@ display:flex;
 flex-flow: row no-wrap;
 align-items:center;
 `
-const Logo = styled.div``
+const Logo = styled.div`
+width:15%;
+`
 
 const Navbar = styled.div`
 display:flex;
@@ -17,24 +19,46 @@ justify-content: flex-start;
 width:100%;
 `
 const NavbarContainer = styled.div`
-width:80%;
-margin-left:5vh;
+width:calc(100vw*80/100);
 `
 const Link = styled.a`
+padding:1vh;
+margin-right: 60px;
+text-decoration:none;
+font-weight: bold;
+color:white;
+&:hover{
+	color:#F38640;
+}
+`
+const HRBar = styled.hr`
+margin:0px;
+border: .5px solid white;
+margin-left: 1vh;
+`
+
+const Branding = styled.div`
+font-family: Roboto;
+font-style: normal;
+font-weight: bold;
+font-size: 20px;
+line-height: 23px;
+padding:1vh;
+color:white
 `
 export default function Header() {
 	return (
 		<Maincontainer>
-			<div>Logo container</div>
+			<Logo>Logo container</Logo>
 			<NavbarContainer>
-				<div>MATHMODELING</div>
-				<hr></hr>
+				<Branding>MATHMODELING</Branding>
+				<HRBar></HRBar>
 				<Navbar>
-					<div>Home</div>
-					<div>About</div>
-					<div>Event</div>
-					<div>Blog</div>
-					<div>Image Gallery</div>
+					<Link href='/'>Home</Link>
+					<Link href='/about'>About</Link>
+					<Link href='/event'>Event</Link>
+					<Link href='/blog'>Blog</Link>
+					<Link href='/image'>Image Gallery	</Link>
 				</Navbar>
 			</NavbarContainer>
 		</Maincontainer>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,52 +1,46 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const All = styled.div`
+background-color:#2C5684
+`
 
 export default function Home() {
-    return (
-        <div>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            Home
-        </div>
-    )
+	return (
+		<All>
+			<div id='explore'>
+				<div></div>
+				<div>
+					<div>GIOI THIEU</div>
+					<div>Gioi thieu Gioi Thieu</div>
+				</div>
+				<div>Explore</div>
+			</div>
+			<div id='next event'>
+				<div>
+					<h3>UONLINE CONTEST</h3>
+					<h4>Dess awifap pawjfpawf pajwifjpawof apjwfijpawf papawfpaf rig seofa[ờ oawfonp awhfoawi awofiapw aowifpfn</h4>
+				</div>
+				<div>Doc them</div>
+				<div>
+					<div>Thoi gian</div>
+					<div>Dia diem</div>
+					<div>Dang ki</div>
+				</div>
+			</div>
+			<div id='Number'>
+				<h2>Nhung con so an tuong</h2>
+				<div>
+					<div></div>
+					<div></div>
+					<div></div>
+					<div></div>
+				</div>
+			</div>
+			<div id='commet'></div>
+			<div id='bk'></div>
+			<div id='partner'></div>
+			<div id='mailrec'></div>
+		</All>
+	)
 }
